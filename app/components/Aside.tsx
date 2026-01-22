d329dd4c13b556c088f35f55640e66d0cf02dd63
RendonnM <srendonm@correo.iue.edu.co>
2026-01-21 21:04:30 -0500

    feat(favicon)✨: add BrandLogo component and replace SVG in Aside header

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { OpenArrowIcon } from "./icons/OpenArrowIcon";
 import { CloseArrowIcon } from "./icons/CloseArrowIcon";
+import { BrandLogo } from "./brand/BrandLogo";
 
 export function Aside() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,17 +12,7 @@ export function Aside() {
     >
       <header className="h-5 mb-8 w-full flex text-white justify-center">
         <h1 className="text-xl flex gap-2 font-bold">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="m17.285 10.668l5.215 3.323l-5.252 3.346L12 13.993l-5.248 3.344L1.5 13.99l5.215-3.323L1.5 7.346L6.752 4L12 7.343L17.248 4L22.5 7.346zm-.074 0L12 7.348l-5.211 3.32L12 13.988zM6.786 18.446l5.252-3.346l5.252 3.346l-5.252 3.346z"
-            />
-          </svg>
+          <BrandLogo />
           <span className={`${isOpen ? "" : "hidden"}`}>Partner Portal</span>
         </h1>
       </header>
